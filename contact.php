27f671dfb8a8506904eae3cff3d76769a84107eb
georgedevasia0 <georgedevasia0@george.github.com>
2020-09-10 01:28:51 +0530

    Assisii-10-09-20

---
+++ contact.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/vendor/aos/aos.css">
+    <link rel="stylesheet" href="assets/css/contact.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        .logo1 {
+            width: 150px !important;
+            margin-top: -30px !important;
+        }
+
+        @media (max-width: 992px) {
+            .logo1 {
+                width: 150px !important;
+                margin-top: -30px !important;
+            }
+
+            @media (max-width: 768px) {
+                .logo1 {
+                    width: 200px !important;
+                    margin-top: -35px !important;
+                }
+
+                .logo {
+                    height: 150px;
+                }
+
+                #header #logo img {
+                    max-height: 70px;
+                }
+            }
+        }
+        
+    </style>
+</head>
+<body style="font-family: 'Raleway', sans-serif;">
+ <!-- ======= Header Starts Here ======= -->  
+ <?php include'header.html';?>
+ <!-- ======= Header Starts Here ======= -->
+<section class="ftco-section bg-light">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-12">
+				<div class="wrapper mt-5">
+					
+					<div class="row no-gutters" style="border-radius:5px;">
+						<div class="col-md-7 col-sm-12" data-aos="fade-right">
+							<div class="contact-wrap w-100  p-4">
+								<h2 class="mb-4 text-dark font-weight-bold text-center">Get In Touch</h2>
+								<form method="POST" id="contactForm" name="contactForm" class="contactForm">
+									<div class="row">
+										<div class="col-md-12">
+											<div class="form-group">
+												<label class="label font-weight-bold text-dark" for="name">Your Name</label>
+												<input type="text" class="form-control" name="name" id="name" placeholder="Name">
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="form-group">
+												<label class="label font-weight-bold text-dark" for="subject">Email</label>
+												<input type="text" class="form-control" name="subject" id="subject" placeholder="youremail@gmail.com">
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="form-group">
+												<label class="label font-weight-bold text-dark" for="#">Message</label>
+												<textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message"></textarea>
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="form-group">
+												<input type="submit" value="Send Message" class="btn col-lg-3 reverse p-2" style="border-radius:50px;">
+												<div class="submitting"></div>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						    <div class="col-md-5 d-flex align-items-stretch" data-aos="fade-left">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15731.050454612483!2d76.7263471!3d9.7012908!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfacb18c5187e2654!2sAssissi%20Institute%20of%20Foreign%20Languages!5e0!3m2!1sen!2sin!4v1572667057847!5m2!1sen!2sin"
+                                     style="border:0;width:100%;" allowfullscreen=""></iframe>  
+                            </div>
+						</div>
+                    </div>
+                    
+
+                    <div class="row  mt-5 pt-5">
+						<div class="col-md-3" data-aos="fade-up" data-aos-delay="50">
+							<div class="dbox w-100 text-center">
+		        		        <div class="icon d-flex align-items-center justify-content-center">
+		        			        <span class="fa fa-map-marker"></span>
+		        		        </div>
+		        		        <div class="text">
+			                        <p><span>Address:</span> Opposite of St.Alphonsa Church Garden, Bharananganam, Kerala 686578</p>
+			                    </div>
+		                    </div>
+				        </div>
+				        <div class="col-md-3" data-aos="fade-up" data-aos-delay="100">
+				        	<div class="dbox w-100 text-center">
+		                        <div class="icon d-flex align-items-center justify-content-center">
+		                	        <span class="fa fa-phone"></span>
+		                        </div>
+		                        <div class="text">
+			                        <p><span>Phone:</span> <a href="tel://1234567920">099612 46648</a></p>
+			                    </div>
+		                    </div>
+				        </div>
+				        <div class="col-md-3" data-aos="fade-up" data-aos-delay="150">
+				            <div class="dbox w-100 text-center">
+		                    	<div class="icon d-flex align-items-center justify-content-center">
+		                    		<span class="fa fa-paper-plane"></span>
+		                    	</div>
+		                    	<div class="text">
+			                      <p><span>Email:</span> <a href="mailto:info@yoursite.com">assisiifl@gmail.com</a></p>
+			                    </div>
+		                    </div>
+				        </div>
+				        <div class="col-md-3" data-aos="fade-up" data-aos-delay="200">
+				            <div class="dbox w-100 text-center">
+		                    	<div class="icon d-flex align-items-center justify-content-center">
+		                    		<span class="fa fa-globe"></span>
+		                    	</div>
+		                    	<div class="text">
+			                      <p><span>Website</span> <a href="#">www.assisiinstitute.org</a></p>
+			                    </div>
+		                    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- ======= Footer Starts Here ======= -->
+<?php include'footer.html';?>
+<!-- ======= Footer Starts Here ======= -->
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/wow/wow.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/superfish/superfish.min.js"></script>
+<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+
+
+<script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ gallery.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery</title>
+    <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/vendor/aos/aos.css">
+    <link rel="stylesheet" href="assets/css/gallery.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        .logo1 {
+            width: 150px !important;
+            margin-top: -30px !important;
+        }
+
+        @media (max-width: 992px) {
+            .logo1 {
+                width: 150px !important;
+                margin-top: -30px !important;
+            }
+
+            @media (max-width: 768px) {
+                .logo1 {
+                    width: 200px !important;
+                    margin-top: -35px !important;
+                }
+
+                .logo {
+                    height: 150px;
+                }
+
+                #header #logo img {
+                    max-height: 70px;
+                }
+            }
+        }
+        
+    </style>
+</head>
+<body style="font-family: 'Raleway', sans-serif;">
+    <!-- ======= Header Starts Here ======= -->  
+    <?php include'header.html';?>
+    <!-- ======= Header Starts Here ======= -->
+
+
+        <div class="popular-directorya-area mt-5 bg-light  border-bottom section-padding40 fix">
+            <div class="container">
+                <div class="directory-active row">
+                    <!-- Single -->
+                    <div class="properties pb-20 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="50">
+                        <div class="properties__card">
+                            <div class="properties__img overlay1">
+                                <a href="album.php"><img src="assets/img/slide1.jpg" alt="" height="210" width="300"></a>
+                                <div class="img-text">
+                                    <span>28-02-2020</span>
+                                    <!--<span>Closed</span>-->
+                                </div>
+                                <div class="icon">
+                                    <img src="assets/img/categori_icon1.png" alt=""> 
+                                </div>
+                            </div>
+                            <div class="properties__caption">
+                                <h3><a href="album.php">Album</a></h3>
+                                <p style="margin-bottom:25px;">Let's uncover the College Festival Album</p>
+                            </div>
+                            <div class="properties__footer d-flex justify-content-between align-items-center">
+                                <div class="restaurant-name col-lg-12">
+                                    <span><h5 style="float:left;line-height:1.5"><i class="fa fa-bank pr-2"></i>College Festival</h5><a href="album.php"><button class="btn px-3" style="float:right;background-color:#FDD35F;border-radius:20px;">View</button></a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="properties pb-20 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="100">
+                        <div class="properties__card">
+                            <div class="properties__img overlay1">
+                                <a href="rankers.php"><img src="assets/img/img_2.jpg" alt="" height="210" width="300"></a>
+                                <div class="img-text">
+                                    <span>28-02-2020</span>
+                                    <!--<span>Closed</span>-->
+                                </div>
+                                <div class="icon">
+                                    <img src="assets/img/categori_icon1.png" alt=""> 
+                                </div>
+                            </div>
+                            <div class="properties__caption">
+                                <h3><a href="rankers.php">Rank Holders</a></h3>
+                                <p style="margin-bottom:25px;">View our recent Rank holders.</p>
+                            </div>
+                            <div class="properties__footer d-flex justify-content-between align-items-center">
+                                <div class="restaurant-name col-lg-12">
+                                    <span><h5 style="float:left;line-height:1.5"><i class="fas fa-user-graduate pr-2"></i>Our Winners</h5><a href="rankers.php"><button class="btn px-3" style="float:right;background-color:#FDD35F;border-radius:20px;">View</button></a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="properties pb-20 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="150">
+                        <div class="properties__card">
+                            <div class="properties__img overlay1">
+                                <a href="youtube.php"><img src="assets/img/img_1.jpg" alt=""></a>
+                                <div class="img-text">
+                                    <span>28-02-2020</span>
+                                    <!--<span>Closed</span>-->
+                                </div>
+                                <div class="icon">
+                                    <img src="assets/img/categori_icon1.png" alt=""> 
+                                </div>
+                            </div>
+                            <div class="properties__caption">
+                                <h3><a href="youtube.php">Videos</a></h3>
+                                <p style="margin-bottom:25px;">Our Functional and Educative videos</p>
+                            </div>
+                            <div class="properties__footer d-flex justify-content-between align-items-center">
+                                <div class="restaurant-name col-lg-12">
+                                    <span><h5 style="float:left;line-height:1.5"><i class="fab fa-youtube pr-2"></i>Youtube Videos</h5><a href="youtube.php"><button class="btn px-3" style="float:right;background-color:#FDD35F;border-radius:20px;">View</button></a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="properties pb-20 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+                        <div class="properties__card">
+                            <div class="properties__img overlay1">
+                                <a href="youtube.php"><img src="assets/img/img_1.jpg" alt=""></a>
+                                <div class="img-text">
+                                    <span>28-02-2020</span>
+                                    <!--<span>Closed</span>-->
+                                </div>
+                                <div class="icon">
+                                    <img src="assets/img/categori_icon1.png" alt=""> 
+                                </div>
+                            </div>
+                            <div class="properties__caption">
+                                <h3><a href="youtube.php">Videos</a></h3>
+                                <p style="margin-bottom:25px;">Our Functional and Educative videos</p>
+                            </div>
+                            <div class="properties__footer d-flex justify-content-between align-items-center">
+                                <div class="restaurant-name col-lg-12">
+                                    <span><h5 style="float:left;line-height:1.5"><i class="fab fa-youtube pr-2"></i>Youtube Videos</h5><a href="youtube.php"><button class="btn px-3" style="float:right;background-color:#FDD35F;border-radius:20px;">View</button></a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    <!-- ======= Footer Starts Here ======= -->
+    <?php include'footer.html';?>
+    <!-- ======= Footer Starts Here ======= -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/wow/wow.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/superfish/superfish.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+
+
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
